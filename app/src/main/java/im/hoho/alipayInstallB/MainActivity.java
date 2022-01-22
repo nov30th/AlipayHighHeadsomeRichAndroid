75ea41b2931cc5281354ab355331e465bc8fa31c
HOHO`` <nov30th@gmail.com>
2022-01-22 19:16:49 +0800

    cleaned code and fix maven deps

--- app/src/main/java/im/hoho/alipayInstallB/MainActivity.java
+++ app/src/main/java/im/hoho/alipayInstallB/MainActivity.java
@@ -1,21 +1,16 @@
 package im.hoho.alipayInstallB;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    //    private Switch switchEnableMatchSee;
-//    private Switch switchEnableSoul;
-//
-//
-//    private SharedPreferences sp;
-//    private SharedPreferences.Editor editor;
-    private ListView listview;
-    private TextView textView;
+public class MainActivity extends Activity {
 
     //
 
@@ -25,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        listview = (ListView) findViewById(R.id.listview);
-        textView = (TextView) findViewById(R.id.textView2);
+        ListView listview = (ListView) findViewById(R.id.listview);
+        TextView textView = (TextView) findViewById(R.id.textView2);
 
         textView.setText("2.4.0-Gamma");
 
@@ -37,12 +32,28 @@ public class MainActivity extends AppCompatActivity {
                 "支付宝付款显示钻石会员背景。",
                 "自定义付款码皮肤（可随机变换）.",
                 "没有界面控件，请参考文档操作。",
+                "",
+                "文档及源代码项目地址:",
+                "https://github.com/nov30th/AlipayHighHeadsomeRichAndroid",
+
         };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_expandable_list_item_1,
                 listContent);
         listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = (String) ((TextView) view).getText();
+                if (text.contains("https")){
+                    Uri uri = Uri.parse(text);
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 }
